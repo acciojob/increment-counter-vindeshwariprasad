ab87d68f95f9e55513665d90127e7b4c4ed39a51
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 06:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 
 function okay() {
-	let a = document.getElementById("z");
+	let a = document.getElementById("incrementBtn");
 	alert(parseInt(a.innerHTML));
 	a.innerHTML = parseInt(a.innerHTML)+1;
 	
