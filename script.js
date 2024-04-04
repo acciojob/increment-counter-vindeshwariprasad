5d4970b2364e8b2c5a990ba38f44cf8d7a9cc95a
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-04-05 01:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //your JS code here. If required.
 
-function okay() {
-	let a = document.getElementById("incrementBtn");
+
+	let a = document.getElementById("counter");
+	let b = document.getElementById("incrementBtn");
+	b.addEventListener("click", () => {
 	alert(parseInt(a.innerHTML));
 	a.innerHTML = parseInt(a.innerHTML)+1;
+	});
 	
-}
+	
+
 
